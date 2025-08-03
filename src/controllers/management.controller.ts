25a3fcd3cb251808aa94ae12d15dda4bd4d6d2dc
Ky Walker <cadecanh@gmail.com>
2025-08-02 19:52:55 -0600

    feat: add staff endpoint

--- src/controllers/management.controller.ts
+++ src/controllers/management.controller.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -19,11 +20,28 @@ export const getAllEmployees = async (_req: Request, res: Response) => {
 
 export const editEmployee = async (req: Request, res: Response) => {
   const { userId } = (req as any).user as { userId?: string };
-  const { name, start_price, colour } = req.body;
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  // Query the current user
+  const currentUser = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!currentUser) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  // Check if user is admin
+  if (!currentUser?.isAdmin) {
+    return res.status(403).json({ error: "You do not have permission" });
+  }
+  const { staff_id, name, start_price, colour } = req.body;
 
   try {
     const updatedEmployee = await prisma.user.update({
-      where: { id: userId },
+      where: { id: staff_id },
       data: {
         name,
         start_price,
@@ -42,5 +60,53 @@ export const editEmployee = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Failed to update employee", error);
     res.status(500).json({ message: "Failed to update employee" });
+  }
+};
+
+export const addEmployee = async (req: Request, res: Response) => {
+  try {
+    const { userId } = (req as any).user as { userId?: string };
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // Query the current user
+    const currentUser = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!currentUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Check if user is admin
+    if (!currentUser?.isAdmin) {
+      return res.status(403).json({ error: "You do not have permission" });
+    }
+
+    const { email, password, name, colour, start_price, phone_number } =
+      req.body;
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashed,
+        name,
+        role: "employee",
+        colour,
+        start_price,
+        isAdmin: false,
+        phone_number,
+      },
+    });
+
+    return res
+      .status(201)
+      .json({ id: user.id, email: user.email, role: user.role });
+  } catch (error) {
+    console.error("Error creating employee:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
--- src/routes/management.routes.ts
+++ src/routes/management.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authenticate, authorize } from "../middleware/auth";
 import {
+  addEmployee,
   editEmployee,
   getAllEmployees,
 } from "../controllers/management.controller";
@@ -10,5 +11,6 @@ const router = Router();
 router.use(authenticate, authorize(["admin", "employee"]));
 router.get("/employees", getAllEmployees);
 router.put("/employees/edit", editEmployee);
+router.post("/employees/add", addEmployee);
 
 export default router;
