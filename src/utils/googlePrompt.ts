5c222485b3400787434a747222837a599e877987
hudo-ng <hmdong901026@gmail.com>
2026-04-07 11:46:35 -0600

    changed gem v1 - v1beta

--- src/utils/googlePrompt.ts
+++ src/utils/googlePrompt.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel(
   { model: "gemini-1.5-flash" },
-  { apiVersion: "v1" },
+  { apiVersion: "v1beta" },
 );
 
 export const generateAIReply = async (
