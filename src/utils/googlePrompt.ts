2934a40a2c939227efc08e936602bfdc5b7cb245
hudo-ng <hmdong901026@gmail.com>
2026-04-07 12:37:05 -0600

    model 2.0 v1

--- src/utils/googlePrompt.ts
+++ src/utils/googlePrompt.ts
@@ -2,8 +2,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel(
-  { model: "gemini-1.5-flash-latest" },
-  { apiVersion: "v1beta" },
+  { model: "gemini-2.0-flash" },
+  { apiVersion: "v1" },
 );
 
 export const generateAIReply = async (
